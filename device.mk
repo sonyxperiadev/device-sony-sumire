203595701cb99bfb917cb5747b88a41a56a85b44
David <Davidteri91@gmail.com>
2016-05-25 00:09:19 +0200

    Sumire: Build InCallUI Signed-off-by: David Viteri <davidteri91@gmail.com>

--- device.mk
+++ device.mk
@@ -51,8 +51,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     nfc_nci.sumire
 
-# SimToolKit
+# Telephony Packages (AOSP)
 PRODUCT_PACKAGES += \
+    InCallUI \
     Stk
 
 PRODUCT_AAPT_CONFIG := large
